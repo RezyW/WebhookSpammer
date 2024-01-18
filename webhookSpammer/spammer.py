1c1947af87b37a2d2f6850072ca0af2fa26831f9
RezyW <156921238+RezyW@users.noreply.github.com>
2024-01-18 19:13:20 +0200

    Add files via upload

---
+++ webhookSpammer/spammer.py
@@ -0,0 +1,17 @@
+import requests
+import time
+
+def send_message(webhook_url, message, delay, message_count):
+    for _ in range(message_count):
+        data = {'content': message}
+        response = requests.post(webhook_url, json=data)
+
+
+        time.sleep(delay)
+
+webhook_url = "https://discord.com/api/webhooks/1197588154014769236/qXJmlZLUDAV6zIvWmi1p9dFjp3jok4e0bLV-7Kc9jaJmDqKxyoSqVRSmD3uXYxn3qUGI"
+message = "RezyW is the best @everyone"
+delay = 0 
+amount_of_messages = 5  
+
+send_message(webhook_url, message, delay, amount_of_messages)
